2dc1fb40812dd50381855af76611ac43da2fd528
ka5ta <kasta.urbanska@gmail.com>
2020-05-24 20:38:12 +0200

    starting, getting ready with Spring, Lombok, Hibernate, REST

---
+++ src/main/java/com/example/VTracker/VTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.VTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VTracker/VTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
