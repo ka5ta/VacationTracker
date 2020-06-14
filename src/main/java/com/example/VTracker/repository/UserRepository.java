352811ffa3c3c0c3c1138fd1c388443d8e19deff
ka5ta <kasta.urbanska@gmail.com>
2020-06-14 16:39:50 +0200

    Adding ORDER BY to GET requests for users. FindAll - Ordered by in the name of method, FindByKeyword - ordered by placed in native SQL query

--- src/main/java/com/example/VTracker/repository/UserRepository.java
+++ src/main/java/com/example/VTracker/repository/UserRepository.java
@@ -1,14 +1,29 @@
 package com.example.VTracker.repository;
 
 import com.example.VTracker.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User,Integer> {
 
-    List<User> findByLastnameOrName(String lastname, String name);
-    //List<User> deleteByAdUserID(String adUserID);
+
+
     Optional <User> findByAdUserID(String adUserID);
+
+    Iterable<User> findByOrderByLastnameAsc();
+
+
+    @Query(value="SELECT * FROM users u " +
+            "WHERE u.name COLLATE utf8_general_ci LIKE %:keyword% COLLATE utf8_general_ci " +
+            "OR u.lastname COLLATE utf8_general_ci LIKE %:keyword% COLLATE utf8_general_ci " +
+            "ORDER BY u.lastname ASC", nativeQuery = true)
+    List<User> findUsersByKeyword(@Param("keyword") String keyword);
+
+
 }
+
