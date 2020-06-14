52d8fc0c9583cb58aef88d26a88a3be091d85759
ka5ta <kasta.urbanska@gmail.com>
2020-06-14 16:41:45 +0200

    Create method 'findUserByAdUserID' so it can be used in two places in code for deleting and finding user. Throws 'NoSuchUserException'.

--- src/main/java/com/example/VTracker/services/UserService.java
+++ src/main/java/com/example/VTracker/services/UserService.java
@@ -4,9 +4,12 @@ import com.example.VTracker.entities.User;
 import com.example.VTracker.errorAPI.NoSuchUserException;
 import com.example.VTracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
 import java.util.Optional;
 
 @Service
@@ -17,8 +20,13 @@ public class UserService {
 
     @Transactional
     public void deleteByAdUserID(String adUserID) throws NoSuchUserException {
+        User user = findUserByAdUserID(adUserID);
+        userRepository.delete(user);
+    }
+
+    public User findUserByAdUserID(String adUserID) throws NoSuchUserException {
         Optional<User> optionalUser = userRepository.findByAdUserID(adUserID);
         User user = optionalUser.orElseThrow(() -> new NoSuchUserException(adUserID));
-        userRepository.delete(user);
+        return user;
     }
 }
