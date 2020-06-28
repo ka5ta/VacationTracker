459228ba632dea29af8ebe7abae7ed864dde549a
ka5ta <kasta.urbanska@gmail.com>
2020-06-28 18:16:54 +0200

    Create BankHoliday entity,

---
+++ src/main/java/com/example/VTracker/entities/BankHoliday.java
@@ -0,0 +1,29 @@
+package com.example.VTracker.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name="holidays")
+public class BankHoliday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private LocalDate day;
+
+    @NotEmpty
+    @JoinColumn(name="country_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Country country;
+}
