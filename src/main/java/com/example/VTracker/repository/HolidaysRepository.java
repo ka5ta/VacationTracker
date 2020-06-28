8a93a40bf54c909fd71c69d88bfe18d276c2419a
ka5ta <kasta.urbanska@gmail.com>
2020-06-28 18:19:11 +0200

    Create Holiday repository with a Query method to find all Holidays by country and date ranges from database

---
+++ src/main/java/com/example/VTracker/repository/HolidaysRepository.java
@@ -0,0 +1,23 @@
+package com.example.VTracker.repository;
+
+import com.example.VTracker.entities.BankHoliday;
+import com.example.VTracker.entities.Country;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+
+public interface HolidaysRepository extends CrudRepository<BankHoliday, Integer> {
+
+    @Query("SELECT h FROM BankHoliday h " +
+            "WHERE h.country = :country AND h.day BETWEEN :startDate AND :endDate" )
+    Collection<BankHoliday> findAllHolidaysByCountryAndDateRange(
+            @Param("country") Country country,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
+
+}
