4b79d50a5eb0a4a1ed7fd22d13db624ada1c7005
ka5ta <kasta.urbanska@gmail.com>
2020-06-28 18:23:16 +0200

    Creating tests which checks if the relevant day is a weekend day or not. To do is to test "countLeaveDays" from LeaveService.class

--- src/test/java/com/example/VTracker/VTrackerApplicationTests.java
+++ src/test/java/com/example/VTracker/VTrackerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.VTracker;
 
+import com.example.VTracker.services.LeaveService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class VTrackerApplicationTests {
+public class VTrackerApplicationTests {
+
+	@Autowired
+	private LeaveService leaveService;
 
 	@Test
-	void contextLoads() {
+	public void isWeekendTest() {
+		LocalDate date = LocalDate.of(2020, 6,28);
+		assertTrue(leaveService.isWeekend(date));
+
 	}
+
+	@Test
+	public void isNotWeekendTest() {
+		LocalDate date = LocalDate.of(2020, 6,29);
+		assertFalse(leaveService.isWeekend(date));
+	}
+
+	@Test
+	public void isCountWeekDaysCorrectTest() {
+
+//todo https://www.baeldung.com/spring-jpa-test-in-memory-database
+	}
+
 
 }
